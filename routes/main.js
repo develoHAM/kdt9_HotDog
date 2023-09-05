ccbe7916959d78ecbeb87a182b4537e6f9dee1be
kangseokjooo <zpckq69@naver.com>
2023-09-05 19:15:21 +0900

    feat:login

--- routes/main.js
+++ routes/main.js
@@ -6,9 +6,10 @@ router.get('/', (req, res) => {
 });
 const controller=require('../controller/Cuser')
 router.get('/', controller.main);
-
 router.get('/signup',controller.get_signup);
-
-router.post('/login',controller.post_signin)
+router.post('/login',controller.post_signin);
+router.get('/mypage',controller.get_mypage);
+router.post('/mypage',controller.post_mypage);
+router.patch('/mypage',controller.mypage);
 router.post('/signup',controller.post_signup)
 module.exports=router;
