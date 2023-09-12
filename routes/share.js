4913cb01a926cd89157006977c8836f5f2e591d4
applicants-backend <tyser@naver.com>
2023-09-12 15:52:23 +0900

    css change

--- routes/share.js
+++ routes/share.js
@@ -4,16 +4,20 @@ const router = express.Router();
 const multer = require('multer');
 const AWS = require("aws-sdk");
 const multerS3 = require('multer-s3');
-// AWS.config.loadFromPath(__dirname + "/../config/awsconfig.json");
 
+AWS.config.update({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: 'ap-northeast-2',
+});
 // const aws = require('aws-sdk'); // aws 설정을 위한 모듈
 
 const s3 = new AWS.S3()
 
-const imageUpload = multer({
+const upload = multer({
   storage: multerS3({
-    s3: s3, // s3 인스턴스
-    bucket: 'kdt9hotdog-bucket', // 버킷 name
+    s3: s3, // 접근 권한, // s3 인스턴스
+    bucket: 'kdt9hotdog', // 버킷 name
     acl: 'public-read', // 파일 접근권한 (public-read로 해야 업로드 파일 공개)
     metadata: function (req, file, cb) {
       cb(null, { fieldname: file.fieldname });
@@ -39,7 +43,7 @@ const imageUpload = multer({
 
 router.get('/', controller.get_main)
 router.get('/user', controller.get_User)
-router.post('/shareCommit', imageUpload.single('dynamic_file') , controller.post_shareCommit)
+router.post('/shareCommit', upload.single('dynamic_file') , controller.post_shareCommit)
 router.patch('/edit/:id', controller.patch_editShare)
 router.delete('/delete', controller.delete_share)
 
